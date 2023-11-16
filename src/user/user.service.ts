6f407df8d1b2f3cb7de49e1caf926bd068122fb3
faridrusdyanto <faridrusdyanto@gmail.com>
2023-11-16 22:39:13 +0700

    registration

---
+++ src/user/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma.service';
+import { hash } from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(dto: CreateUserDto) {
+    const user = await this.prisma.user.create({
+      data: {
+        ...dto,
+        password: await hash(dto.password, 10)
+      }
+    });
+
+    const { password, ...userWithoutPassword } = user;
+    const responseMessage = 'User successfully registered';
+    return { message: responseMessage, user: userWithoutPassword };
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
